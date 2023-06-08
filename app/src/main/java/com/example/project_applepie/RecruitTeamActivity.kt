f31b94e24350c8e52fef1d8647d9fb8385a6fc04
LeeSeokCheol <lsc991123@gmail.com>
2023-06-08 19:26:59 +0900

    팀원 지원

--- app/src/main/java/com/example/project_applepie/RecruitTeamActivity.kt
+++ app/src/main/java/com/example/project_applepie/RecruitTeamActivity.kt
@@ -7,9 +7,12 @@ import android.widget.ArrayAdapter
 import com.bumptech.glide.Glide
 import com.example.project_applepie.databinding.ActivityRecruitTeamBinding
 import com.example.project_applepie.model.boardDetailBoard
+import com.example.project_applepie.model.dao.applyVolunteer
 import com.example.project_applepie.model.recuit
 import com.example.project_applepie.retrofit.ApiService
+import com.example.project_applepie.retrofit.domain.BasicResponse
 import com.example.project_applepie.retrofit.domain.BoardDetailResponse
+import com.example.project_applepie.sharedpref.SharedPref
 import com.example.project_applepie.utils.Url
 import retrofit2.Call
 import retrofit2.Callback
@@ -28,6 +31,7 @@ class RecruitTeamActivity : AppCompatActivity() {
         val intent = intent
         val recruitData = intent.getSerializableExtra("data") as recuit?
         val bid = recruitData?.id
+        val uid = SharedPref.getUserId(this)
 
         val url = Url.BASE_URL
         val retrofit = Retrofit.Builder()
@@ -60,11 +64,30 @@ class RecruitTeamActivity : AppCompatActivity() {
 
         val getRole = resources.getStringArray(R.array.select_role)
         val arrayAdapter = ArrayAdapter(this,R.layout.dropdown_item, getRole)
+        var role = 0
         rtBinding.actvRole.setAdapter(arrayAdapter)
 
         //드롭박스 선택 인원수 확인
         rtBinding.actvRole.setOnItemClickListener { adapterView, view, i, l ->
+            role = i;
             Log.d("로그","역할 선택 : $i")
+        }
+
+        rtBinding.btnApply.setOnClickListener {
+            val av = applyVolunteer(bid, Integer.parseInt(uid), role, "")
+            server.applyVolunteer(av).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>
+                ) {
+                    if(response.isSuccessful){
+                        Log.d("로그 - 성공","${response.body().toString()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                    Log.d("로그 - 서버에러","${t.localizedMessage}")
+                }
+
+            })
         }
 
     }
